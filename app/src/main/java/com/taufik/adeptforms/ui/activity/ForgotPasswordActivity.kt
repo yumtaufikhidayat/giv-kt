9473dcb377f55b5e70bd337ec27605a63604162c
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-09-09 07:54:55 +0700

    Adding forgot password.

--- app/src/main/java/com/taufik/adeptforms/ui/activity/ForgotPasswordActivity.kt
+++ app/src/main/java/com/taufik/adeptforms/ui/activity/ForgotPasswordActivity.kt
@@ -1,24 +1,33 @@
 package com.taufik.adeptforms.ui.activity
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
 import com.taufik.adeptforms.R
 import com.taufik.adeptforms.databinding.ActivityForgotPasswordBinding
-import kotlinx.android.synthetic.main.activity_login.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityForgotPasswordBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         setWindowNotificationBackground()
+
+        initFirebaseAuth()
+
+        setResetPassword()
 
         setSignIn()
 
@@ -33,6 +42,59 @@ class ForgotPasswordActivity : AppCompatActivity() {
         windowNotificationBackground.statusBarColor = ContextCompat.getColor(this, R.color.colorYellow1)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             windowNotificationBackground.setDecorFitsSystemWindows(true)
+        }
+    }
+
+    private fun initFirebaseAuth() {
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun setResetPassword() {
+        binding.apply {
+            btnResetPassword.setOnClickListener {
+
+                val email = etEmail.text.toString().trim()
+
+                if (email.isEmpty()) {
+                    etEmail.error = "Email can't be blank"
+                    etEmail.requestFocus()
+                    return@setOnClickListener
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    etEmail.error = "Insert valid email"
+                    etEmail.requestFocus()
+                    return@setOnClickListener
+                }
+
+                resetPassword(email)
+            }
+        }
+    }
+
+    private fun resetPassword(email: String) {
+        auth.sendPasswordResetEmail(email).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    confirmResetPassword()
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun confirmResetPassword() {
+        AlertDialog.Builder(this).also { builder ->
+            builder.setTitle("Reset email confirm!")
+                .setMessage("Url to reset password has sent. Check your email to reset password.")
+                .setPositiveButton("Sign In") { _: DialogInterface?, _: Int ->
+                    Intent(this, LoginActivity::class.java).also {
+                        it.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(it)
+                    }
+                }
+                .setCancelable(false)
+                .create()
+                .show()
         }
     }
 
