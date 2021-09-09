9a4611b5758b808466207b72866b25d001c57a97
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-09-09 07:24:00 +0700

    Updating splash screen activity to handle login user.

--- app/src/main/java/com/taufik/adeptforms/ui/activity/SplashscreenActivity.kt
+++ app/src/main/java/com/taufik/adeptforms/ui/activity/SplashscreenActivity.kt
@@ -7,12 +7,14 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
 import com.taufik.adeptforms.R
 import com.taufik.adeptforms.databinding.ActivitySplashscreenBinding
 
 class SplashscreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashscreenBinding
+    private lateinit var auth: FirebaseAuth
     private lateinit var handler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +22,15 @@ class SplashscreenActivity : AppCompatActivity() {
         binding = ActivitySplashscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initFirebaseAuth()
+
         setWindowNotificationBackground()
 
         setSplashScreen()
+    }
+
+    private fun initFirebaseAuth() {
+        auth = FirebaseAuth.getInstance()
     }
 
     /**
@@ -39,9 +47,17 @@ class SplashscreenActivity : AppCompatActivity() {
     private fun setSplashScreen() {
         handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+            if (auth.currentUser == null) {
+                Intent(this, LoginActivity::class.java).also { intent ->
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                }
+            } else if (auth.currentUser!!.isEmailVerified){
+                Intent(this, MainActivity::class.java).also { intent ->
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                }
+            }
         }, 1000)
     }
 }
