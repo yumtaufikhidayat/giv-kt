3790a8a4b37af18909ee75edbd2fd59f23d8a16e
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-09-13 09:53:25 +0700

    Adding logout feature.

--- app/src/main/java/com/taufik/adeptforms/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/taufik/adeptforms/ui/fragment/SettingsFragment.kt
@@ -1,16 +1,21 @@
 package com.taufik.adeptforms.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import com.taufik.adeptforms.databinding.FragmentSettingsBinding
+import com.taufik.adeptforms.ui.activity.LoginActivity
 
 class SettingsFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +28,27 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initFirebase()
+
+        setSignOut()
+    }
+
+    private fun initFirebase() {
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun setSignOut() {
+        binding.apply {
+            cardSignOut.setOnClickListener {
+                auth.signOut()
+                Intent(requireActivity(), LoginActivity::class.java).also {
+                    it.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(it)
+                }
+                Toast.makeText(requireActivity(), "Successfully logout", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onDestroyView() {
